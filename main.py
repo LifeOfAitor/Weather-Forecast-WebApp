d198809e42cc10cfd1becf2e8c7b895b25d1d20d
LifeOfAitor <aitorgaillard99@gmail.com>
2024-08-12 10:51:27 +0200

    improve frontend

--- main.py
+++ main.py
@@ -4,6 +4,11 @@ title = st.title("WEATHER FORECAST")
 city = st.text_input(label="", placeholder="Select a city", key="city")
 days = st.slider(label="Forecast Days", key="days", min_value=1, max_value=5)
 data_type = st.selectbox(label="Type of data to visualize",
-                         options=["Temperature", "Images", "Both"])
-
-
+                         options=["Temperature", "Sky", "Both"])
+if city:
+    if data_type == "Both":
+        st.subheader(f"Temperature and sky for the next {days} days in "
+                     f"{city.capitalize()}")
+    else:
+        st.subheader(f"{data_type} for the next {days} days in "
+                     f"{city.capitalize()}")
