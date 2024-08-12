79f3646d7f4f84456cb90c96d3c42d176be30e5a
LifeOfAitor <aitorgaillard99@gmail.com>
2024-08-12 18:05:56 +0200

    manage days count posible errors

--- backend.py
+++ backend.py
@@ -11,14 +11,18 @@ def get_data(place, days, info_type):
     response = requests.get(url)
     data = response.json()
     filtered_data = data["list"]
-    prompt_day = filtered_data[0]["dt_txt"].split(" ")[0]
-    for i in range(len(filtered_data)):
-        while filtered_data[i]["dt_txt"].split(" ")[0] == prompt_day:
-            i += 1
-        filtered_data = filtered_data[:i+((days-1)*8)]
-        break
-    return filtered_data
+    if days < 5:
+        prompt_day = filtered_data[0]["dt_txt"].split(" ")[0]
+        for i in range(len(filtered_data)):
+            while filtered_data[i]["dt_txt"].split(" ")[0] == prompt_day:
+                i += 1
+            filtered_data = filtered_data[:i+((days-1)*8)]
+            break
+        return filtered_data
+    else:
+        return filtered_data
+
 
 
 if __name__ == "__main__":
-    print(get_data("Tokyo", 2, "temperature"))
+    print(get_data("Tokyo", 4, "temperature"))
